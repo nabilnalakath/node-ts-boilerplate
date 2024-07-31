ae8b131ba09b6c0ba9a2fc25beb2022c395f3474
Nabil Mohammed Nalakath <nabilnalakath.git@gmail.com>
2024-08-01 01:05:53 +0530

    level 2

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 import { app, port } from './app';
-import connectDB from './config/database';
-
 /**
  * Function to start the server.
  * Logs the port on which the server is running.
@@ -11,24 +9,5 @@ const startServer = (): void => {
   });
 };
 
-/**
- * Connect to the database and start the server.
- * If the connection fails, logs the error and exits the process.
- */
-const startServerWithDB = async (): Promise<void> => {
-  try {
-    await connectDB();
-    startServer();
-  } catch (err) {
-    console.error('Failed to connect to the database', err);
-    process.exit(1);
-  }
-};
-
-// Uncomment one of the following lines depending on the requirement:
-
-// To start the server with database connection:
-startServerWithDB();
-
-// To start the server without database connection:
-// startServer();
+// To start the server
+startServer();
