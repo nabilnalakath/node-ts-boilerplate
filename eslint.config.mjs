17fc9beff16ff0ae614316cd48f6474744710b79
Nabil Mohammed Nalakath <nabilnalakath.git@gmail.com>
2024-07-31 23:02:15 +0530

    First commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,6 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    ignores: ['node_modules', 'dist'],
+    ignores: ['node_modules', 'dist', 'docs']
   }
 ];
--- src/routes/hello.ts
+++ src/routes/hello.ts
@@ -2,6 +2,21 @@ import { Router, Request, Response } from 'express';
 
 const router = Router();
 
+/**
+ * GET /hello
+ *
+ * @function
+ * @memberof module:routes/hello
+ * @name get/hello
+ *
+ * @description
+ * Returns a JSON object with a "Hello, World!" message.
+ *
+ * @param {Request} req - Express request object.
+ * @param {Response} res - Express response object.
+ *
+ * @returns {void}
+ */
 router.get('/', (req: Request, res: Response) => {
   res.json({ message: 'Hello, World!' });
 });
